9076232af76ef39768da61ade42bac89cb679ccf
Florian Maas <florian.maas@ingka.ikea.com>
2022-06-20 14:40:13 +0200

    formatting

--- main/backend.tf
+++ main/backend.tf
@@ -1,6 +1,6 @@
 terraform {
- backend "gcs" {
-   bucket  = "bucket-tfstate-f38f0b132659d977"
-   prefix  = "terraform/state"
- }
+  backend "gcs" {
+    bucket = "bucket-tfstate-f38f0b132659d977"
+    prefix = "terraform/state"
+  }
 }
